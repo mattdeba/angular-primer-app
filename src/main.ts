e978e4adbaeddddae00315350d7b17016e48bd8d
matthieu debarge <debarge.matthieu@gmail.com>
2023-02-20 13:09:37 +0100

    3.43

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
-let val1 : string | undefined ;
-let val2 : string | undefined = "London" ;
-let val3 : number | undefined = 0 ;
-let coalesced1 = val1 ?? "valeur de repli";
-let coalesced2 = val2 ?? "valeur de repli" ;
-let coalesced3 = val3 ?? 100 ;
-console.log(`Résultat 1 : ${coalesced1}`) ;
-console.log(`Résultat 2 : ${coalesced2}`) ;
-console.log(`Résultat 3 : ${coalesced3}`) ;
+let count: number | undefined | null = 100;
+if (count != null && count != undefined) {
+  let result1: string = count.toFixed(2);
+  console.log(`Result 1: ${result1}`);
+}
+let result2: string | undefined = count?.toFixed(2);
+console.log(`Result 2: ${result2}`);
+
