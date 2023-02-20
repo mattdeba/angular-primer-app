7ce4bc7266aa840eacb73b6735ea5ec64f5e6c4f
matthieu debarge <debarge.matthieu@gmail.com>
2023-02-20 12:03:57 +0100

    3.22

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log("Hello") ;
+function myFunction(param: number | string) {
+  if (typeof(param) == "number" || typeof(param) == "string") {
+    if(typeof (param) == "number") {
+      let fixed = param.toFixed(2);
+      console.log("My result: " + fixed);
+    }
+  } else {
+    throw ("Expected a number or a string: " + param)
+  }
+}
+myFunction(1);
+myFunction("London");
