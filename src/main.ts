eab3122b467da3a73e652028897e3c8f8a294ae3
matthieu debarge <debarge.matthieu@gmail.com>
2023-02-20 12:57:43 +0100

    3.41

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
-let person1 = "Alice" ;
-let person2 : string|undefined = "Bob"
-person2 = null;
+let val1 : string | undefined ;
+let val2 : string | undefined = "London" ;
+let val3 : number | undefined = 0 ;
+let coalesced1 = val1 ?? "valeur de repli";
+let coalesced2 = val2 ?? "valeur de repli" ;
+let coalesced3 = val3 ?? 100 ;
+console.log(`Résultat 1 : ${coalesced1}`) ;
+console.log(`Résultat 2 : ${coalesced2}`) ;
+console.log(`Résultat 3 : ${coalesced3}`) ;
