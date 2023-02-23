ab155f5e70a529b16293ac02fe6006699937fbf3
matthieu debarge <debarge.matthieu@gmail.com>
2023-02-23 10:18:52 +0100

    4.20

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,8 @@ let boots = {
 }
 console.log(`Name: ${hat.name}, Price: ${hat.price}`);
 console.log(`Name: ${boots.name}, Price: ${boots.price}`);
+function printDetails(product : { name: string, price: number}) {
+  console.log(`Name: ${product.name}, Price: ${product.price}`);
+}
+printDetails(hat);
+printDetails(boots);
