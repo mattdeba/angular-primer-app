2bdb58ceac57a4c2fb692e509688b0ff5e747dd9
matthieu debarge <debarge.matthieu@gmail.com>
2023-02-23 09:46:58 +0100

    4.13

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,1 @@
-let myArray : (number | string | boolean)[] = [] ;
-myArray[0] = 100 ;
-myArray[1] = "Adam" ;
-myArray[2] = true ;
+let myArray : (number | string | boolean)[] = [100, "Adam", true] ;
