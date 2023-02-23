73a5ed8245bc414d83265a59bdbd0b778bfcf417
matthieu debarge <debarge.matthieu@gmail.com>
2023-02-23 10:25:03 +0100

    4.22

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,18 @@ let hat = {
 };
 let boots = {
   name: "Boots",
-  price: 100
+  price: 100,
+  category: "Snow Gear"
 }
 console.log(`Name: ${hat.name}, Price: ${hat.price}`);
 console.log(`Name: ${boots.name}, Price: ${boots.price}`);
-function printDetails(product : { name: string, price: number}) {
-  console.log(`Name: ${product.name}, Price: ${product.price}`);
+function printDetails(product : { name: string, price: number, category?: string}) {
+  if (product.category != undefined) {
+    console.log(`Name: ${product.name}, Price: ${product.price}, ` +
+      `Category: ${product.category}`);
+  } else {
+    console.log(`Name: ${product.name}, Price: ${product.price}`);
+  }
 }
 printDetails(hat);
 printDetails(boots);
