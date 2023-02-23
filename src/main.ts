cb0b9846093762b052783067f8d6a5c1ae514da0
matthieu debarge <debarge.matthieu@gmail.com>
2023-02-23 09:54:09 +0100

    4.16

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-let myArray : (number | string | boolean)[] = [100, "Adam", true] ;
-myArray[0] = "Tuesday";
-let val = myArray[0];
-console.log(`Value: ${val}`);
+let myArray: (number | string | boolean)[] = [100, "Adam", true];
+for (let i = 0; i < myArray.length; i++) {
+  console.log("Index " + i + ": " + myArray[i]);
+}
+console.log("---");
+myArray.forEach((value, index) => console.log("Index " + index + ": " + value));
