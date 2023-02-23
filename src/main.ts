8ffa7684b9a29d804d4ddbc53664ca4b9314b177
matthieu debarge <debarge.matthieu@gmail.com>
2023-02-23 09:58:19 +0100

    4.17

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 let myArray: (number | string | boolean)[] = [100, "Adam", true];
-for (let i = 0; i < myArray.length; i++) {
-  console.log("Index " + i + ": " + myArray[i]);
-}
-console.log("---");
-myArray.forEach((value, index) => console.log("Index " + index + ": " + value));
+let otherArray = [...myArray, 200, "Bob", false];
+// for (let i = 0; i < myArray.length; i++) {
+//     console.log("Index " + i + ": " + myArray[i]);
+// }
+// console.log("---");
+otherArray.forEach((value, index) => console.log("Index " + index + ": " + value));
