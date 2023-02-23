029cc98be745439e9b412882d13b1099a426ce51
matthieu debarge <debarge.matthieu@gmail.com>
2023-02-23 10:14:03 +0100

    4.18

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 let myArray: (number | string | boolean)[] = [100, "Adam", true];
 let otherArray = [...myArray, 200, "Bob", false];
-// for (let i = 0; i < myArray.length; i++) {
-//     console.log("Index " + i + ": " + myArray[i]);
-// }
-// console.log("---");
-otherArray.forEach((value, index) => console.log("Index " + index + ": " + value));
+let sum: number = otherArray
+  .filter(val => typeof(val) == "number")
+  .reduce((accumulator: number, currentValue) => accumulator+(currentValue as number)**2, 0)
+console.log(`Sum: ${sum}`);
