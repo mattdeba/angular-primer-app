e755bddbbb4b1170250c26ed4f73cd323e59b70d
matthieu debarge <debarge.matthieu@gmail.com>
2023-02-23 10:57:32 +0100

    4.29

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,5 @@
-class Product {
-  constructor(public name: string, public price: number, public category?: string) {
-  }
-  printDetails() {
-    if (this.category != undefined) {
-      console.log(`Name: ${this.name}, Price: ${this.price}, ` +
-        `Category: ${this.category}`);
-    } else {
-      console.log(`Name: ${this.name}, Price: ${this.price}`);
-    }
-  }
-}
-class DiscountProduct extends Product {
-  constructor(name: string, price: number, private discount: number) {
-    super(name, price - discount);
-  }
-}
-let hat = new DiscountProduct("Hat", 100, 10);
-let boots = new Product("Boots", 100, "Snow Gear");
-console.log(`Name: ${hat.name}, Price: ${hat.price}`);
-console.log(`Name: ${boots.name}, Price: ${boots.price}`);
-hat.printDetails();
-boots.printDetails();
-
-console.log(`Hat is a Product? ${hat instanceof Product}`);
-console.log(`Hat is a DiscountProduct? ${hat instanceof DiscountProduct}`);
-console.log(`Boots is a Product? ${boots instanceof Product}`);
-console.log(`Boots is a DiscountProduct? ${boots instanceof DiscountProduct}`);
-
+import { Name, WeatherLocation } from "./modules/NameAndWeather" ;
+let name = new Name("Matthieu", "Debarge") ;
+let loc = new WeatherLocation("sunny", "Paris") ;
+console.log(name.nameMessage) ;
+console.log(loc.weatherMessage) ;
